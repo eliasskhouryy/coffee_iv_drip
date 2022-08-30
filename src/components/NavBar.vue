be29bfa60353310f6850d55f0a91df1f7b307f3e
Elias Khoury <eliaskhouryy@hotmail.com>
2022-08-30 15:33:29 +1000

    wheel done

---
+++ src/components/NavBar.vue
@@ -0,0 +1,124 @@
+<template>
+	<nav>
+		<div class="hamburger-menu">
+			<input id="menu__toggle" type="checkbox" />
+			<label class="menu__btn" for="menu__toggle">
+				<span></span>
+			</label>
+			<ul class="menu__box">
+				<li><a href="">Home</a></li>
+				<li><a href="">Home</a></li>
+				<li><a href="">Home</a></li>
+				<li><a href="">Home</a></li>
+			</ul>
+			<h3>Coffee IV Drip</h3>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: 'nav-bar',
+};
+</script>
+
+<style scoped lang="scss">
+$red: #e0785f;
+$white: #f4f1de;
+$lightBlue: #e8f0ff;
+$darkBlue: #3d405b;
+$green: #81b29a;
+$beige: #f2cc8f;
+
+nav {
+	position: fixed;
+	top: 0;
+	left: 0;
+	background-color: $lightBlue;
+	width: 100%;
+	padding: 30px;
+
+	#menu__toggle {
+		opacity: 0;
+	}
+	#menu__toggle:checked + .menu__btn > span {
+		transform: rotate(45deg);
+	}
+	#menu__toggle:checked + .menu__btn > span::before {
+		top: 0;
+		transform: rotate(0deg);
+	}
+	#menu__toggle:checked + .menu__btn > span::after {
+		top: 0;
+		transform: rotate(90deg);
+	}
+	#menu__toggle:checked ~ .menu__box {
+		left: 0 !important;
+	}
+	.menu__btn {
+		position: fixed;
+		top: 40px;
+		left: 35px;
+		width: 26px;
+		height: 26px;
+		cursor: pointer;
+		z-index: 1;
+		display: inline-block;
+	}
+	.menu__btn > span,
+	.menu__btn > span::before,
+	.menu__btn > span::after {
+		display: block;
+		position: absolute;
+		width: 100%;
+		height: 2px;
+		background-color: $darkBlue;
+		color: $darkBlue;
+		transition-duration: 0.25s;
+	}
+	.menu__btn > span::before {
+		content: '';
+		top: -8px;
+	}
+	.menu__btn > span::after {
+		content: '';
+		top: 8px;
+	}
+	.menu__box {
+		display: block;
+		position: fixed;
+		top: 0;
+		left: -100%;
+		width: 300px;
+		height: 100%;
+		margin: 0;
+		padding: 80px 0;
+		list-style: none;
+		background-color: white;
+		box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.4);
+		transition-duration: 0.25s;
+	}
+	.menu__item {
+		display: block;
+
+		color: $darkBlue;
+
+		font-size: 20px;
+		font-weight: 300;
+		text-decoration: none;
+		transition-duration: 0.25s;
+	}
+	.menu__item:hover {
+		background-color: #eaffd0;
+		color: $darkBlue;
+	}
+
+	h3 {
+		display: inline-block;
+		margin: 0;
+	}
+	a {
+		text-decoration: none;
+	}
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,68 +1,64 @@
 <template>
-  <div class="home">
-    <button>Subscribe</button>
-
-    <div class="box page1">
-      <TopPage msg="Welcome to Your Vue.js App"/>
-
-    </div>
-    <div class="box page3">
-      dummy text
-    </div>
-    <div class="box page3">
-
-      <EmailForm />
-    </div>
-  </div>
+	<div>
+		<NavBar />
+		<div class="home">
+			<div class="box page1">
+				<TopPage />
+			</div>
+			<div class="box page3">dummy text</div>
+			<div class="box page3">
+				<EmailForm />
+			</div>
+			<button>Subscribe</button>
+		</div>
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import TopPage from '@/components/TopPage.vue'
-import EmailForm from '@/components/EmailForm.vue'
-
+import TopPage from '@/components/TopPage.vue';
+import EmailForm from '@/components/EmailForm.vue';
+import NavBar from '@/components/NavBar.vue';
 export default {
-  name: 'HomeView',
-  components: {
-    TopPage,
-    EmailForm
-  },
-  data: function () {
-    return {
-      variable1: '',
-    }
-  },
-}
-
-
+	name: 'HomeView',
+	components: {
+		NavBar,
+		TopPage,
+		EmailForm,
+	},
+	data: function () {
+		return {
+			variable1: '',
+		};
+	},
+	methods: {
+		buttonShower: function () {},
+	},
+};
 </script>
 
-<style lang="scss">
-
+<style scoped lang="scss">
 * {
-  box-sizing: border-box;
+	box-sizing: border-box;
 }
 
-  .home {
-    overflow: auto;
-    .box {
-      height: 100vh;
-      max-width: 100vw;
-    }
-    button {
-      position: fixed;
-      right: 0px;
-      bottom: 20px; 
-      -ms-transform: translate(-50%, -50%);
-      transform: translate(-50%, -50%);
-      z-index: 10;
-      height: 80px;
-      width: 80px;
-      border-radius: 50%;
-      border: none;
-      box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-
-    }
-  }
-  
+.home {
+	margin-top: 50px;
+	.box {
+		height: 100vh;
+		max-width: 100vw;
+	}
+	button {
+		position: fixed;
+		right: 0px;
+		bottom: 30px;
+		-ms-transform: translate(-50%, -50%);
+		transform: translate(-50%, -50%);
+		z-index: 10;
+		height: 30px;
+		border-radius: 5px;
+		border: none;
+		box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+	}
+}
 </style>
