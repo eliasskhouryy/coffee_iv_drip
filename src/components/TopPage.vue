a5620c4db63e276b78d08084302a1cf6974dae53
Elias Khoury <eliaskhouryy@hotmail.com>
2022-08-30 13:05:58 +1000

    button added

--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'TopPage',
   props: {
     msg: String
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="home">
+    <button>Subscribe</button>
+
     <div class="box page1">
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <TopPage msg="Welcome to Your Vue.js App"/>
+
     </div>
     <div class="box page3">
       dummy text
     </div>
     <div class="box page3">
       fummy text
+
     </div>
-    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TopPage from '@/components/TopPage.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    TopPage
   },
   data: function () {
     return {
@@ -35,11 +38,29 @@ export default {
 
 <style lang="scss">
 
-  .home {
+* {
+  box-sizing: border-box;
+}
 
+  .home {
+    overflow: auto;
     .box {
       height: 100vh;
       max-width: 100vw;
+    }
+    button {
+      position: fixed;
+      right: 0px;
+      bottom: 20px; 
+      -ms-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      z-index: 10;
+      height: 80px;
+      width: 80px;
+      border-radius: 50%;
+      border: none;
+      box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
     }
   }
   
