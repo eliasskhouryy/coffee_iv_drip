1e7930d386d35024f3bebb7a9dce125a9410bd58
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-08-30 13:38:13 +1000

    added headings and name formfield to the emailform

--- src/components/EmailForm.vue
+++ src/components/EmailForm.vue
@@ -1,5 +1,13 @@
 <template>
     <form action="#" @submit.prevent="onSubmit">
+      <section>
+        <h3> Subscribe to our newsletter </h3>
+        <p> Coffee IV drip delivered straight to your door. </p>
+        <p> How does it work? </p>
+      </section>
+
+        <label> Full Name </label>
+        <input type="text" placeholder="John Jones" v-model="name" required />
         <label> Email: </label>
         <input type="email" placeholder="john@ga.co" v-model="email" required/>
         <input type="submit"/>
@@ -11,7 +19,8 @@ export default {
   name: 'email-form',
   data: function(){
     return {
-      email: ''
+      email: '',
+      name: '',
     }
   }
 }
